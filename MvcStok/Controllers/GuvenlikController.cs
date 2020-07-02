c565b1d903464526b887fb299fa4a7299e2c3712
SevdeSare <58533625+SevdeSare@users.noreply.github.com>
2020-07-02 19:03:50 +0300

    MvcYonetim

---
+++ MvcStok/Controllers/GuvenlikController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MvcStok.Models.Entity;
+
+
+namespace MvcStok.Controllers
+{
+    public class GuvenlikController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+
+        // GET: Guvenlik
+        [AllowAnonymous]
+        public ActionResult GirisYap()
+        {
+            ViewBag.Mesaj1 = "Bilgilerinizi giriniz.";
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult GirisYap(TBLADMIN t1)
+        {
+            var bilgiler = db.TBLADMIN.FirstOrDefault(x => x.AD == t1.AD && x.SİFRE == t1.SİFRE);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.AD,false);
+                return RedirectToAction("Index","Kategori");
+            }
+            else
+            {
+                ViewBag.Mesaj2 = "Geçersiz Kullanıcı Adı veya Şifre Girdiniz!";
+                return View();
+            }
+                         
+        }
+        
+        public ActionResult CikisYap()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("GirisYap");
+        }
+    }
+}
---
+++ MvcStok/Controllers/SatisController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {       
+            var degerler = db.TBLSATISLAR.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            //var satis = db.TBLSATISLAR.Find(p.ADET);
+            var satis = db.TBLURUNLER.Find(p.URUN);
+            for (int i = 0; i > p.ADET; i++)
+            {
+                satis.STOK--;
+            }
+
+            satis.STOK--;
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var satis = db.TBLSATISLAR.Find(id);
+            db.TBLSATISLAR.Remove(satis);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SatisGetir(int id)
+        {
+            var sat = db.TBLSATISLAR.Find(id);
+            return View("SatisGetir", sat);
+        }
+        public ActionResult Guncelle(TBLSATISLAR p1)
+        {
+            var sat = db.TBLSATISLAR.Find(p1.SATISID);
+            //sat.URUN = p1.URUN;
+            //sat.MUSTERI = p1.MUSTERI;
+            sat.FIYAT = p1.FIYAT;
+            sat.ADET = p1.ADET;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
